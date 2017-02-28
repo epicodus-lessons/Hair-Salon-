f28ffcd25ad87256f632708359364899be3f94ec
felixop8 <felixop8@gmail.com>
2017-02-27 19:48:47 -0800

    add class stylist and set constructor

---
+++ src/Stylist.php
@@ -0,0 +1,15 @@
+<?php
+Class Stylist
+{
+    private $id;
+    private $name;
+    private $telephone;
+    private $availability;
+    function __construct($id = null, $name, $telephone, $availability) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->telephone = $telephone;
+    }
+
+}
+    ?>
